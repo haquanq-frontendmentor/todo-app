649625303b108533e3ec5b0944e69613d3f6650a
Ha Quanq <quangha0120010@gmail.com>
2025-10-09 01:59:46 +0700

    Add icon exports

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -2,3 +2,8 @@ export { default as BackgroundDesktopDark } from "./bg-desktop-dark.jpg";
 export { default as BackgroundDesktopLight } from "./bg-desktop-light.jpg";
 export { default as BackgroundMobileDark } from "./bg-mobile-dark.jpg";
 export { default as BackgroundMobileLight } from "./bg-mobile-light.jpg";
+
+export { default as CheckIcon } from "./icon-check.svg";
+export { default as CrossIcon } from "./icon-cross.svg";
+export { default as MoonIcon } from "./icon-moon.svg";
+export { default as SunIcon } from "./icon-sun.svg";
