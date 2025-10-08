2934146a281ed40e2e1b6fb217f592a05686b466
Ha Quanq <quangha0120010@gmail.com>
2025-10-09 01:40:05 +0700

    Add static images

---
+++ src/assets/images/index.ts
@@ -0,0 +1,4 @@
+export { default as BackgroundDesktopDark } from "./bg-desktop-dark.jpg";
+export { default as BackgroundDesktopLight } from "./bg-desktop-light.jpg";
+export { default as BackgroundMobileDark } from "./bg-mobile-dark.jpg";
+export { default as BackgroundMobileLight } from "./bg-mobile-light.jpg";
