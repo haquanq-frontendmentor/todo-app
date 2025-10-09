feb7bd27d5e8c44502999dd2a07202161221e7be
Ha Quanq <quangha0120010@gmail.com>
2025-10-09 22:42:49 +0700

    Add page Header component

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,21 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { BackgroundDesktopDark, BackgroundDesktopLight } from "./assets/images";
+import Header from "./components/Header.vue";
+import { useThemeStore } from "./stores/themeStore";
 
-<template></template>
+const { isDark } = useThemeStore();
+</script>
+
+<template>
+  <div class="dark:bg-navy-950 relative min-h-screen bg-gray-500 pb-[11.875rem] md:pb-[7.1875rem]">
+    <div
+      class="absolute top-0 right-0 left-0 h-[clamp(12.5rem,6.5363rem+25.4453vw,18.75rem)] [&_img]:h-full [&_img]:w-full [&_img]:object-cover"
+    >
+      <img v-show="!isDark()" :src="BackgroundDesktopLight" alt="" />
+      <img v-show="isDark()" :src="BackgroundDesktopDark" alt="" />
+    </div>
+    <div class="relative z-10 mx-auto w-[min(100vw-3rem,33.75rem)] bg-gray-900">
+      <Header />
+    </div>
+  </div>
+</template>
