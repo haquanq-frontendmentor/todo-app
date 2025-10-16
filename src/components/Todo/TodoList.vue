a0bdcc60a5230aa0338558bbe68f91d583a4cbd7
Ha Quanq <quangha0120010@gmail.com>
2025-10-16 23:53:59 +0700

    Add sortable drag-n-drop feature for todos

--- src/components/Todo/TodoList.vue
+++ src/components/Todo/TodoList.vue
@@ -1,21 +1,29 @@
 <script setup lang="ts">
-import { computed } from "vue";
+import { storeToRefs } from "pinia";
+import { ref } from "vue";
+import { VueDraggableNext } from "vue-draggable-next";
 import { useTodoStore } from "../../stores/todoStore";
 import TodoItem from "./TodoItem.vue";
 
-const { removeTodo, getFilteredTodos } = useTodoStore();
+const todoStore = useTodoStore();
 
-const todos = computed(() => getFilteredTodos());
+const dragging = ref<boolean>(false);
+
+const { todos } = storeToRefs(todoStore);
 </script>
 
 <template>
-  <ul class="text-navy-900 rounded-md *:first:rounded-t-md dark:text-purple-300">
-    <TodoItem
-      v-for="(todo, index) in todos"
-      :todo="todo"
-      :key="index + todo.content"
-      @remove-click="removeTodo(index)"
-    />
+  <ul class="text-navy-900 rounded-md *:first:rounded-t-md dark:text-purple-300" :class="dragging && 'select-none'">
+    <VueDraggableNext
+      v-model="todos"
+      item-key="id"
+      tag="transition-group"
+      :animation="200"
+      @start="dragging = true"
+      @end="dragging = false"
+    >
+      <TodoItem v-for="(todo, index) in todos" :todo="todo" @remove-click="todoStore.removeTodo(index)" />
+    </VueDraggableNext>
     <li
       class="flex h-50 items-center justify-center border-b border-gray-300 px-4 text-center text-base leading-6 dark:border-purple-600"
       v-if="todos.length === 0"
